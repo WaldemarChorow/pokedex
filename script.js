77297e58c059e5ddaade0fec97e3bf29389b998c
WaldemarChorow <chorow.w@gmail.com>
2026-03-30 15:43:27 +0200

    add: overlay, syles, fonts. design: pokemonCards, nav, main and header section

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 
+let currentOffset = 0;
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function loadPokemon(){
-    let url = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
+    document.getElementById('loading-screen').classList.remove('d-none');
+    let url = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=${currentOffset}`;
     let response = await fetch(url);    
     let responseAsJson = await response.json();
     let pokemonList = responseAsJson.results;
@@ -9,14 +16,45 @@ async function loadPokemon(){
         const element = pokemonList[i];
         await renderSinglePokemon(element.url);     
      }
+     await timeout(1000);
+     document.getElementById('loading-screen').classList.add('d-none');
 }
 
 async function renderSinglePokemon(url){
     let responseDetail = await fetch(url);
-    let pokemonDetail = await responseDetail.json();
-    let pokemonCardHtml = getPokemonCardTemplate(pokemonDetail.name, pokemonDetail.id, pokemonDetail.sprites.front_default, pokemonDetail.types[0].type.name);
+    let pokemonDetail = await responseDetail.json();    
+    let type1 = pokemonDetail.types[0].type.name;
+    let type2 = "";
+    if (pokemonDetail.types.length > 1) {
+        type2 = pokemonDetail.types[1].type.name;
+    }
+    let pokemonCardHtml = getPokemonCardTemplate(pokemonDetail.name, pokemonDetail.id, pokemonDetail.sprites.front_default, type1, type2);
     document.getElementById('pokedex').innerHTML += pokemonCardHtml;
     console.log(pokemonDetail);
 }
 
 loadPokemon();
+
+function loadMorePokemon(){
+    currentOffset += 20;
+    loadPokemon();
+}
+
+ async function openOverlay(id){
+    document.getElementById('overlay').classList.remove('d-none')
+    let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    let response = await fetch(url);    
+    let pokemonDetail = await response.json();
+    let type1 = pokemonDetail.types[0].type.name;
+    let type2 = "";
+    if (pokemonDetail.types.length > 1) {
+        type2 = pokemonDetail.types[1].type.name;
+    }
+    document.getElementById('overlay-content').innerHTML = getPokemonBigCardTemplate(
+    pokemonDetail.name, pokemonDetail.id, pokemonDetail.sprites.other['official-artwork'].front_default, type1, type2
+    );   
+}
+
+function closeOverlay() {
+    document.getElementById('overlay').classList.add('d-none');    
+}
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,12 +1,23 @@
-function getPokemonCardTemplate(pokemonName, pokemonId, pokemonImage, pokemonType1){
+function getPokemonCardTemplate(pokemonName, pokemonId, pokemonImage, pokemonType1, pokemonType2){
     return /*html*/`
-        <div class="pokemonCard text-capitalize ${pokemonType1}">
-            <h1>${pokemonName} #${pokemonId}</h1>
+        <div onclick="openOverlay(${pokemonId})" class="pokemonCard text-capitalize ${pokemonType1}">
+            <h2>${pokemonName} #${pokemonId}</h2>
             <img src="${pokemonImage}" alt="" srcset="">
             <div>
-                <h2>Types</h2>
-                <p>${pokemonType1}</p>
+                <h3>Types</h3>
+                <p>${pokemonType1} ${pokemonType2}</p>
             </div>
         </div>
     `
+}
+
+function getPokemonBigCardTemplate(pokemonName, pokemonId, pokemonImage, type1, type2) {
+    return /*html*/`
+        <div class="bigPokemonCard text-capitalize ${type1}">
+            <button class="close-btn" onclick="closeOverlay()">X</button>
+            <h2>${pokemonName} #${pokemonId}</h2>
+            <img src="${pokemonImage}" alt="pokemon">
+            <p>Typen: ${type1} ${type2}</p>
+        </div>
+    `;
 }
