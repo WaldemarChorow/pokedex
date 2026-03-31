70805a5c9170f9f9e1c4e612440e1c6237ed9362
WaldemarChorow <chorow.w@gmail.com>
2026-03-31 08:19:31 +0200

    add: elementColors.css. change: template.js

--- scripts/templates.js
+++ scripts/templates.js
@@ -1,14 +1,19 @@
-function getPokemonCardTemplate(pokemonName, pokemonId, pokemonImage, pokemonType1, pokemonType2){
+function getPokemonCardTemplate(pokemonName, pokemonId, pokemonImage, type1, type2) {
     return /*html*/`
-        <div onclick="openOverlay(${pokemonId})" class="pokemonCard text-capitalize ${pokemonType1}">
-            <h2>${pokemonName} #${pokemonId}</h2>
-            <img src="${pokemonImage}" alt="" srcset="">
-            <div>
-                <h3>Types</h3>
-                <p>${pokemonType1} ${pokemonType2}</p>
+        <div id="pokemon-card-${pokemonId}" onclick="openOverlay(${pokemonId})" class="pokemonCard ${type1}">
+            <span class="card-id">#${pokemonId}</span>       
+            <h2 class="card-name">${pokemonName}</h2>
+            <div class="card-main-content">
+                <div class="card-types">
+                    <span class="type-badge">${type1}</span>
+                    ${type2 ? `<span class="type-badge">${type2}</span>` : ''}
+                </div>
+                <div class="card-image-container">
+                    <img src="${pokemonImage}" alt="${pokemonName}">
+                </div>
             </div>
         </div>
-    `
+    `;
 }
 
 function getPokemonBigCardTemplate(pokemonName, pokemonId, pokemonImage, type1, type2) {
